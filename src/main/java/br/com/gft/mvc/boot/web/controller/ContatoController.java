e22270936fbfc7cdfc3ade13bb12603e6f06d8cb
marlon-santos <marlonergondossantos@gmail.com>
2020-01-22 16:03:03 -0300

    firs commit

--- src/main/java/br/com/gft/mvc/boot/web/controller/ContatoController.java
+++ src/main/java/br/com/gft/mvc/boot/web/controller/ContatoController.java
@@ -3,10 +3,12 @@ package br.com.gft.mvc.boot.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
-public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "/home";
+public class ContatoController {
+    @GetMapping("/contato")
+    public String cadastro() {
+        return "./contato/contato";
     }
+
 }
--- src/main/java/br/com/gft/mvc/boot/web/controller/ProjetoController.java
+++ src/main/java/br/com/gft/mvc/boot/web/controller/ProjetoController.java
@@ -3,10 +3,12 @@ package br.com.gft.mvc.boot.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
-public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "/home";
+public class ProjetoController {
+    @GetMapping("/projeto")
+    public String cadastro() {
+        return "./projeto/projeto";
     }
+
 }
